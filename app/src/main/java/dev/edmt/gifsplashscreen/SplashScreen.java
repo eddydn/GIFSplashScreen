dd938493a8ae95da92cd7f82e4a056f775ebf39d
eddydn <eddydn@gmail.com>
2016-11-21 13:25:03 +0700

    First Commit
    First Commit

---
+++ app/src/main/java/dev/edmt/gifsplashscreen/SplashScreen.java
@@ -0,0 +1,51 @@
+package dev.edmt.gifsplashscreen;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private GifImageView gifImageView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        gifImageView = (GifImageView)findViewById(R.id.gifImageView);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(progressBar.VISIBLE);
+
+        //Set GIFImageView resource
+        try{
+            InputStream inputStream = getAssets().open("splash_screen.gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex)
+        {
+
+        }
+
+        //Wait for 3 seconds and start Activity Main
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashScreen.this.startActivity(new Intent(SplashScreen.this,MainActivity.class));
+                SplashScreen.this.finish();
+            }
+        },3000); // 3000 = 3seconds
+    }
+}
